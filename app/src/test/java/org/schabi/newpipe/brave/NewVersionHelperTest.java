bd02dc870761a54a5289d10b53b745b5c7700ed3
evermind <evermind@tuxfamily.org>
2022-05-03 16:55:10 +0200

    BraveNewPipe: test getAlternativeUrlOnKitkat() instead checkForAlternativeApkUrl()

--- app/src/test/java/org/schabi/newpipe/brave/NewVersionHelperTest.java
+++ app/src/test/java/org/schabi/newpipe/brave/NewVersionHelperTest.java
@@ -1,12 +1,16 @@
 package org.schabi.newpipe.brave;
 
+import android.os.Build;
+
 import com.grack.nanojson.JsonObject;
 import com.grack.nanojson.JsonParser;
-import com.grack.nanojson.JsonParserException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,36 +25,61 @@ public class NewVersionHelperTest {
     public void tearDown() throws Exception {
     }
 
-    @SuppressWarnings({"checkstyle:LineLength", "checkstyle:MethodName"})
-    @Test
-    public void checkJsonConfigWith_alternative_apks() throws JsonParserException {
-        final String stdUrl = "stdUrl";
-        final String expectedAltUrl = "altUrl";
+    private static void setFinalStatic(final Field field, final Object newValue) throws Exception {
+        field.setAccessible(true);
 
-        final String altConfig = "{\"flavors\":{\"github\":{\"stable\":{\"apk\":\"" + stdUrl + "\","
-                + "\"alternative_apks\":[{\"alternative\":\"conscrypt\",\"url\":\"" + expectedAltUrl + "\"}],"
-                + "\"version\":\"1\",\"version_code\":1}}}}";
+        final Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-        final JsonObject obj = JsonParser.object().from(altConfig).getObject("flavors")
-                .getObject("github").getObject("stable");
-        final String result = NewVersionHelper.checkForAlternativeApkUrl(obj, stdUrl);
-
-        assertEquals(expectedAltUrl, result);
+        field.set(null, newValue);
     }
 
     @SuppressWarnings({"checkstyle:LineLength", "checkstyle:MethodName"})
     @Test
-    public void checkJsonConfigWithout_alternative_apks() throws JsonParserException {
+    public void checkJsonDataWith_alternative_apks() throws Exception {
+        final String stdUrl = "stdUrl";
+        final String expectedAltUrl = "altUrl";
+
+        final String withAltJSON = "{\"flavors\":{\"github\":{\"stable\":{\"apk\":\"" + stdUrl + "\","
+                + "\"alternative_apks\":[{\"alternative\":\"conscrypt\",\"url\":\"" + expectedAltUrl + "\"}],"
+                + "\"version\":\"1\",\"version_code\":1}}}}";
+
+        final JsonObject obj = JsonParser.object().from(withAltJSON).getObject("flavors")
+                .getObject("github").getObject("stable");
+
+        // 1. test simulating running on KitKat
+        setFinalStatic(Build.VERSION.class.getField("SDK_INT"), 19);
+        String result = NewVersionHelper.getAlternativeUrlOnKitkat(obj, stdUrl);
+        assertEquals(expectedAltUrl, result);
+
+        // 2. test simulating running > KitKat
+        setFinalStatic(Build.VERSION.class.getField("SDK_INT"), 21);
+        result = NewVersionHelper.getAlternativeUrlOnKitkat(obj, stdUrl);
+        assertEquals(stdUrl, result);
+    }
+
+    @SuppressWarnings({"checkstyle:LineLength", "checkstyle:MethodName"})
+    @Test
+    public void checkJsonDataWithout_alternative_apks() throws Exception {
         final String stdUrl = "stdUrl";
         final String expectedStdUrl = stdUrl;
 
-        final String altConfig = "{\"flavors\":{\"github\":{\"stable\":{\"apk\":\"" + stdUrl + "\","
+        final String withoutAltJSON = "{\"flavors\":{\"github\":{\"stable\":{\"apk\":\"" + stdUrl + "\","
                 + "\"version\":\"1\",\"version_code\":1}}}}";
 
-        final JsonObject obj = JsonParser.object().from(altConfig).getObject("flavors")
+        final JsonObject obj = JsonParser.object().from(withoutAltJSON).getObject("flavors")
                 .getObject("github").getObject("stable");
-        final String result = NewVersionHelper.checkForAlternativeApkUrl(obj, stdUrl);
 
+
+        // 1. test simulating running on KitKat
+        setFinalStatic(Build.VERSION.class.getField("SDK_INT"), 19);
+        String result = NewVersionHelper.getAlternativeUrlOnKitkat(obj, stdUrl);
         assertEquals(expectedStdUrl, result);
+
+        // 2. test simulating running > KitKat
+        setFinalStatic(Build.VERSION.class.getField("SDK_INT"), 21);
+        result = NewVersionHelper.getAlternativeUrlOnKitkat(obj, stdUrl);
+        assertEquals(stdUrl, result);
     }
 }
